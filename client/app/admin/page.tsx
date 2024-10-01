a717df7b9778233e3a484636856a16a37dbed593
Italo Adler <italo.adler@DIGITALCOLLEGE.LOCAL>
2024-10-01 19:53:50 -0300

    Adiciona template base com Next+Shadcn e telas de registro/login

---
+++ client/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default  function Page() { 
+    return ( 
+        
+        <><h1>Outra página</h1>
+        
+        <Button>Olá</Button>
+        
+         </>
+    )
+}
---
+++ client/app/registro/page.tsx
@@ -0,0 +1,10 @@
+import { RegistroUsuario } from '@/components/registro-usuario'
+
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center  min-h-screen bg-gray-100">
+      <RegistroUsuario />
+    </div>
+  )
+}
