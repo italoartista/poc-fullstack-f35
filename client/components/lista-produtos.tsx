e67eaf8a09e0ba491edd43e18f05858b1c769105
MARCOS DANIEL GOMES DANTAS <MARCOS0197@DIGITALCOLLEGE.LOCAL>
2024-10-22 21:48:35 -0300

    inicio de login

---
+++ client/components/lista-produtos.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function ListaProdutos() {
+
+return(
+    <>
+    </>
+)
+
+}
--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const pg = require('pg'); // Importa o módulo pg para interagir com o PostgreSQ
 const { Pool } = pg; // Extrai a classe Pool do módulo pg
 
 // Configura a conexão com o banco de dados PostgreSQL
-const client = new Pool({
+const pool = new Pool({
     user: 'postgres', // Nome de usuário do banco de dados
     host: 'localhost', // Host do banco de dados
     database: 'ecommerce', // Nome do banco de dados
@@ -21,8 +21,15 @@ const port = 3001; // Define a porta em que o servidor irá rodar
 app.use(express.json()); // Middleware para parsear requisições com JSON
 app.use(cors()); // Middleware para habilitar CORS
 
+function criptografarSenha(senha) {
+    const salt = bcrypt.genSaltSync(10); // Gera um salt aleatório
+    return bcrypt.hashSync(senha, salt); // Retorna a senha criptografada
+}
+
+
 // Define a rota POST /registro para registrar novos usuários
 app.post('/registro', (req, res) => {
+    const { email, senha } = req.body; // Extrai email e senha do corpo da requisição
     /* 
         IMPLEMENTAR LÓGICA DE REGISTRO DE USUÁRIO AQUI
         - Validar dados de entrada (email e senha)
@@ -30,10 +37,36 @@ app.post('/registro', (req, res) => {
         - Inserir usuário no banco de dados
         - Retornar resposta apropriada
     */
+   // 
+   const senhaCriptografada = criptografarSenha(senha);
+//    console.log(email, senhaCriptografada);   
+//    res.status(200).send('Usuário registrado com sucesso');
+
+const query = "INSERT INTO usuarios (email, senha, data_registro) VALUES ($1, $2, NOW())"; 
+// Define a query SQL para inserir um usuário no banco de dados
+
+pool.query(query, [email, senhaCriptografada], (error, result) => {
+    if (error) {
+        console.error(error); // Loga o erro no console
+        return res.status(400).send('Erro ao registrar o usuário');
+    }
+    res.status(200).send('Usuário registrado com sucesso'); // Retorna uma resposta de sucesso
 });
 
+});
+
+
+
 // Define a rota POST /login para autenticar usuários
-app.post('/login', (req, res) => {
+app.post('/login',async (req, res) => {
+    const {email,senha} = req.body; // Extrai email e senha do corpo da requisição   
+    
+    if (!email || !senha) {
+        return res.status(400).send('Email e senha são obrigatórios');
+    }
+    try {
+        const query = ""
+    }
     /* 
         IMPLEMENTAR LÓGICA DE LOGIN AQUI
         - Validar dados de entrada (email e senha)
@@ -42,9 +75,6 @@ app.post('/login', (req, res) => {
         - Retornar resposta apropriada
     */
 });
-
-
-
 
 
 // Inicia o servidor na porta definida
