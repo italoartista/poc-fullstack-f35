1fd263797eabfcc98327ed4194a5ea8f4068fe1d
Fabricio Nascimento Moreira <fabricio0748@DIGITALCOLLEGE.LOCAL>
2024-10-22 21:45:35 -0300

    fiz uma rota post que criptografa senha e manda para a database

--- server/app.js
+++ server/app.js
@@ -11,18 +11,44 @@ const { Pool } = pg; // Extrai a classe Pool do módulo pg
 const client = new Pool({
     user: 'postgres', // Nome de usuário do banco de dados
     host: 'localhost', // Host do banco de dados
-    database: 'ecommerce', // Nome do banco de dados
+    database: 'auladb', // Nome do banco de dados
     password: '', // Senha do banco de dados
     port: 5432, // Porta do banco de dados
 });
 
-const port = 3001; // Define a porta em que o servidor irá rodar
+const port = 3000; // Define a porta em que o servidor irá rodar
 
 app.use(express.json()); // Middleware para parsear requisições com JSON
 app.use(cors()); // Middleware para habilitar CORS
 
+
+function criptrografarSenha(senha){
+
+    return bcrypt.hashSync(senha, 10);
+}
+
 // Define a rota POST /registro para registrar novos usuários
 app.post('/registro', (req, res) => {
+    console.log(req.body)
+    const email = req.body.email 
+    const senha = req.body.senha
+    // const { email, senha } = req.body;
+
+    res.status(200).json({ message: 'Dados recebidos com sucesso!' });
+
+    const senhaCripto = criptrografarSenha(senha);
+
+    const query = 'INSERT INTO usuarios (email, senha)  VALUES ($1, $2)';
+
+    client.query(query, [email, senhaCripto], (error, result)=>{
+        if(error) {
+            console.error(error);
+            return res.status(400).send('erro ao registrar usuário');
+        }
+        res.status(201).send('usuario registrado com sucesso');
+    })
+
+
     /* 
         IMPLEMENTAR LÓGICA DE REGISTRO DE USUÁRIO AQUI
         - Validar dados de entrada (email e senha)
